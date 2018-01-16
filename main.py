5fc296c5d0e6a6e815b4ee5d23becf521c90ba11
ravenusmc <mcuddy77@gmail.com>
2018-01-16 16:52:10 -0500

    added login and sign up pages

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ app = Flask(__name__)
 def landing():
     return render_template('landing.html')
 
+#This route will take the user to the login page
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+#This route will take the user to sign up page
+@app.route('/sign_up')
+def signup():
+  return render_template('signup.html')
+
 
 # set the secret key. keep this really secret:
 app.secret_key = 'n3A\xef(\xb0Cf^\xda\xf7\x97\xb1x\x8e\x94\xd5r\xe0\x11\x88\x1b\xb9'
