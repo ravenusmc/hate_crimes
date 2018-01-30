f05831834567df425249036ddc18947f075fe4db
ravenusmc <mcuddy77@gmail.com>
2018-01-30 11:39:15 -0500

    wrote code to display bokeh scatter plot

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 #This file will hold all of the work for the CSV file. 
+from bokeh.charts import Scatter, output_file, show
+from bokeh.plotting import figure, output_file, show
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from nvd3 import scatterChart
 import pandas as pd
 
-import random
 
 
 #This class will handle all of the data manipulation
@@ -23,44 +24,8 @@ class Data():
     def correlation(self):
         print(self.data.corr())
 
-    def scatter(self):
-        #setting up a counter
-        i = 0
-        #This arrays will hold the data 
-        trump = []
-        income = []
-        state = []
-        #Pulling the data from the csv file
-        trump_data = self.data['share_voters_voted_trump']
-        income_data = self.data['median_household_income']
-        state_data = self.data['state']
-        #I then have to loop through both data sets from the csv file 
-        #It will loop through the data sets while the len is less than one of the data 
-        #sets-only need to use one since they are both the same length. 
-        while i < len(trump_data):
-            trump.append(float(trump_data[i]))
-            income.append(float(income_data[i]))
-            state.append(state_data[i])
-            #incrementing the counter by one 
-            i += 1
 
-        #creating the temporary HTML file that i'll use    
-        output_file = open('test_scatterChart.html', 'w')
-        type = "scatterChart"
-        #setting up the chart size
-        chart = scatterChart(name=type, height=350, width=800, x_is_date=False)
-        chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
-    
-        xdata = income
-        ydata = trump
 
-        kwargs1 = {'shape': 'circle', 'size': '1', }
-
-        extra_serie = {"tooltip": {"y_start": "Mike ", "y_end": "" }}
-        chart.add_serie(name="State", y=ydata, x=xdata, extra=extra_serie, **kwargs1)
-        chart.buildhtml()
-        output_file.write(chart.htmlcontent)
-        output_file.close()
 
 """ Things to look at:
 
@@ -74,5 +39,6 @@ class Data():
 
 
 data = Data()
-data.scatter()
+#data.scatter()
+data.bokeh()
 
---
+++ graph.py
@@ -0,0 +1,112 @@
+#This file will handle the making of all of the graphs for the web app. 
+
+#Importing libraries for use in this file
+from bokeh.charts import Bar, Scatter, output_file, show
+from bokeh.io import output_file, show
+from bokeh.plotting import ColumnDataSource, figure, output_file, show
+from bokeh.models import CategoricalColorMapper, HoverTool
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from nvd3 import scatterChart
+import pandas as pd
+
+
+class Graph():
+
+    # def __init__(self):
+    #     self.data = pd.read_csv('data.csv')
+
+    def bokeh(self):
+        output_file("test.html")
+
+        file = 'data.csv'
+        data = pd.read_csv(file)
+
+        hate_crime_data = ColumnDataSource(data) 
+
+        plot = figure(x_axis_label='Median Household Income', y_axis_label='percentage who voted for Trump',
+            plot_width=600, plot_height=500, tools='pan,wheel_zoom,box_zoom,reset,hover,save', 
+            title='Trump Voters VS. Median Household Income')
+
+        plot.circle(x='median_household_income', y='share_voters_voted_trump', source=hate_crime_data, 
+            size=15)
+
+        hover = plot.select_one(HoverTool)
+        hover.tooltips = [('state', '@state'),
+        ('Median Household Income', '@median_household_income'),
+        ('Percentage Voted Trump', '@share_voters_voted_trump ')]
+
+        show(plot)
+
+graph = Graph()
+graph.scatter()
+
+
+
+
+
+
+######## OLD CODE DOWN HERE --- FOR FUTURE REFERENCE #########
+
+    #This method will use NVD3 to make the scatter plot 
+    # def scatter(self):
+    #     #setting up a counter
+    #     i = 0
+    #     #This arrays will hold the data 
+    #     trump = []
+    #     income = []
+    #     state = []
+    #     #Pulling the data from the csv file
+    #     trump_data = self.data['share_voters_voted_trump']
+    #     income_data = self.data['median_household_income']
+    #     state_data = self.data['state']
+    #     #I then have to loop through both data sets from the csv file 
+    #     #It will loop through the data sets while the len is less than one of the data 
+    #     #sets-only need to use one since they are both the same length. 
+    #     while i < len(trump_data):
+    #         trump.append(float(trump_data[i]))
+    #         income.append(float(income_data[i]))
+    #         state.append(state_data[i])
+    #         #incrementing the counter by one 
+    #         i += 1
+
+    #     #creating the temporary HTML file that i'll use    
+    #     output_file = open('test_scatterChart.html', 'w')
+    #     type = "scatterChart"
+    #     #setting up the chart size
+    #     chart = scatterChart(name=type, height=350, width=800, x_is_date=False)
+    #     chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+    
+    #     xdata = income
+    #     ydata = trump
+
+    #     kwargs1 = {'shape': 'circle', 'size': '1', }
+
+    #     extra_serie = {"tooltip": False}
+    #     chart.add_serie(name="State", y=ydata, x=xdata, extra=extra_serie, **kwargs1)
+    #     chart.buildhtml()
+    #     output_file.write(chart.htmlcontent)
+    #     output_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
