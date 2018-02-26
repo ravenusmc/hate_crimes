4a85a4e39323bb70c19e7a622ade8092550ad20d
ravenusmc <mcuddy77@gmail.com>
2018-02-25 19:56:26 -0500

    finished writing method to scrape data

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from csv import writer
 import requests 
 
+
 #This class will take care of all of the scraping that I do. 
 class Scraping():
 
@@ -18,8 +19,8 @@ class Scraping():
         # self.response = requests.get(self.requests_list[0])
 
     #This method will set up the initial url 
-    def setup_soup(self):
-        self.response = requests.get(self.requests_list[0])
+    def setup_soup(self, count):
+        self.response = requests.get(self.requests_list[count])
         self.soup = BeautifulSoup(self.response.text, "html.parser")
         # self.tables = self.soup.find("table")
 
@@ -27,22 +28,61 @@ class Scraping():
     def push_into_csv(self):
         #opening up the csv file that I'll write to
         with open("blog_data.csv", "w") as csv_file:
+            cell_list = ['cell71', 'cell81', 'cell121', 'cell131', 'cell141','cell151', 'cell171',
+            'cell161', 'cell171', 'cell201']
+            #The year will change per page so I need to ensure that each page has the correct
+            #year
+            year_list = ['2010', '2011', '2012', '2013', '2014', '2015', '2016']
             #Creating a csv variable 
             csv_writer = writer(csv_file)
             #This line will write the rows to the CSV file, the header rows. 
             csv_writer.writerow(["year", "total", "anti-black", "anti-jewish", "anti-islamic"])
-            year = self.soup.find('a', {'href': 'https://ucr.fbi.gov/hate-crime/2010'}).get_text()
-            black = self.soup.find(id="cell71").contents[0]
-            #converting from the weird beautiful soup data type to an integer. 
-            black = self.get_number(black)
-            jewish = self.soup.find(id="cell121").get_text()
-            jewish = self.get_number(jewish)
-            islamic = self.soup.find(id="cell151").get_text()
-            islamic = self.get_number(islamic)
-            total = black + jewish + islamic
-            csv_writer.writerow([year, total, black, jewish, islamic])
 
-
+            count = 0
+            while count < len(year_list):
+                self.setup_soup(count)
+                year = self.soup.find('a', {'href': 'https://ucr.fbi.gov/hate-crime/' + year_list[count] }).get_text()
+                if count == 0:
+                    black = self.soup.find(id=cell_list[0]).contents[0] 
+                    #converting from the weird beautiful soup data type to an integer. 
+                    black = self.get_number(black)
+                    jewish = self.soup.find(id=cell_list[2]).get_text()
+                    jewish = self.get_number(jewish)
+                    islamic = self.soup.find(id=cell_list[5]).get_text()
+                    islamic = self.get_number(islamic)
+                    total = black + jewish + islamic
+                    csv_writer.writerow([year, total, black, jewish, islamic])
+                elif count == 1 or count == 2:
+                    black = self.soup.find(id=cell_list[1]).contents[0] 
+                    #converting from the weird beautiful soup data type to an integer. 
+                    black = self.get_number(black)
+                    jewish = self.soup.find(id=cell_list[3]).get_text()
+                    jewish = self.get_number(jewish)
+                    islamic = self.soup.find(id=cell_list[7]).get_text()
+                    islamic = self.get_number(islamic)
+                    total = black + jewish + islamic
+                    csv_writer.writerow([year, total, black, jewish, islamic])
+                elif count == 3 or count == 4:
+                    black = self.soup.find(id=cell_list[1]).contents[0] 
+                    #converting from the weird beautiful soup data type to an integer. 
+                    black = self.get_number(black)
+                    jewish = self.soup.find(id=cell_list[4]).get_text()
+                    jewish = self.get_number(jewish)
+                    islamic = self.soup.find(id=cell_list[6]).get_text()
+                    islamic = self.get_number(islamic)
+                    total = black + jewish + islamic
+                    csv_writer.writerow([year, total, black, jewish, islamic])
+                elif count == 5 or count == 6:
+                    black = self.soup.find(id=cell_list[1]).contents[0] 
+                    #converting from the weird beautiful soup data type to an integer. 
+                    black = self.get_number(black)
+                    jewish = self.soup.find(id=cell_list[6]).get_text()
+                    jewish = self.get_number(jewish)
+                    islamic = self.soup.find(id=cell_list[9]).get_text()
+                    islamic = self.get_number(islamic)
+                    total = black + jewish + islamic
+                    csv_writer.writerow([year, total, black, jewish, islamic])
+                count += 1
 
     #This method will get the numbers from the content of the web scraping. 
     #I was returning a blank line each time I scraped and needed to get rid of that blank 
@@ -64,19 +104,11 @@ class Scraping():
         crime_number = int(''.join(num_list))
         #returning the integer. 
         return crime_number
-
-
-
-
-    # a_tag = article.find("a")
-    # title = a_tag.get_text()
-    # url = a_tag["href"]
-    # date = article.find("time")["datetime"]
-    # csv_writer.writerow([title, url, date])        
+     
 
 
 
 
 test = Scraping()
-test.setup_soup()
+# test.setup_soup(count)
 test.push_into_csv()
